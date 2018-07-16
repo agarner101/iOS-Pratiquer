b9ebde670e1f422bc7d3803f36348e81ae1979c0
Andrew Garner <andrewgarner101@gmail.com>
2018-07-15 19:07:44 -0700

    Bronze Challenge: Another Tab
    Added View Controller

---
+++ WorldTrotter/WorldTrotter/WebPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebPageViewController.swift
+//  WorldTrotter
+//
+//  Created by Andrew Garner on 7/15/18.
+//  Copyright © 2018 Andrew Garner. All rights reserved.
+//
+
+import UIKit
+
+class WebPageViewController: UIViewController {
+    
+}
